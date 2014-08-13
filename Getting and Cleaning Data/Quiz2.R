45df1cec600ceada498869a27984c0ce0ecb699c
Christian Paul Dehli <cpdehli@gmail.com>
2014-08-12 23:20:47 -0400

    Finished quiz 2 - still some code left to be tinkered with.

--- Getting and Cleaning Data/Quiz2.R
+++ Getting and Cleaning Data/Quiz2.R
@@ -1,21 +1,31 @@
 library("httr")
 library("jsonlite")
+library("XML")
 
-oauth_endpoints("github")
-myapp <- oauth_app("github", "d0564c2bebe659279422", "cad43bbd666b010c61a1bed10147b40284c2504c")
-github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
-gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
-stop_for_status(req)
-data <- content(req)
-data <- jsonlite::fromJSON(toJSON(data))
-data[5,2]  #name
-data[5,45] #creation date
+function1 <- function(){
+  oauth_endpoints("github")
+  myapp <- oauth_app("github", "d0564c2bebe659279422", "SECRET_KEY")
+  github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+  gtoken <- config(token = github_token)
+  req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+  stop_for_status(req)
+  data <- content(req)
+  data <- jsonlite::fromJSON(toJSON(data))
+  print(data[5,2])  #name
+  print(data[5,45]) #creation date
+}
 
-con = url("./search.html")   #("http://biostat.jhsph.edu/~jleek/contact.html")
-htmlCode = readLines(con)
-close(con)
-nchar(htmlCode[,10])
-nchar(htmlCode[,20])
-nchar(htmlCode[,30])
-nchar(htmlCode[,100])
+function2 <- function(){  
+  con = url("http://biostat.jhsph.edu/~jleek/contact.html")
+  htmlCode <- readLines(con)
+  close(con)
+   
+  print(nchar(htmlCode[10]))
+  print(nchar(htmlCode[20]))
+  print(nchar(htmlCode[30]))
+  print(nchar(htmlCode[100]))
+}
+
+function3 <- function(){
+  url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+}
